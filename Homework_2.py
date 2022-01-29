3d6b5f7aeaaa06a53033550e3d5344796c0f473a
kirill <kirill.s.b.88@gmail.com>
2022-01-29 20:47:08 +0200

    homework2 with inputs

---
+++ Homework_2.py
@@ -0,0 +1,7 @@
+# inp1 = int(input('value 1: '))
+# inp2 = int(input("value 2: "))
+# print(inp1 // inp2)
+# print(inp1 % inp2)
+guestList = 'Sasha Petya Vasya Kolya'
+guest = input('enter guest name: ')
+print(guest + ' ' in guestList)
