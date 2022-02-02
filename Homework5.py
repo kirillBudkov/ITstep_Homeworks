55ce265ff42509ae6ce444ea4d2f03e601f311af
kirill <kirill.s.b.88@gmail.com>
2022-02-02 21:40:27 +0200

    homework4 with circles

---
+++ Homework5.py
@@ -0,0 +1,12 @@
+# task 1
+# param=int(input('enter parameter: '))
+# for i in range(1, param+1):
+#     print('*'*i)
+# task 2
+# inp1 = int(input('enter start of range: '))
+# inp2 = int(input('enter end of range: '))
+# _sum=0
+# for i in range(inp1, inp2+1):
+#     if i%5==0 and i%2!=0:
+#         _sum+=i
+# print(_sum)
