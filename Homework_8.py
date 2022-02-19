3a2fb7dcd4c0d3de9a7da8f083bb40a1a8b477bd
kirill <kirill.s.b.88@gmail.com>
2022-02-19 21:00:26 +0200

    Homework 7 list and set

---
+++ Homework_8.py
@@ -0,0 +1,32 @@
+# task 1
+# guests=[]
+# while True:
+#     guest = input('enter name: ')
+#     if guest!='all':
+#         guests.append(guest)
+#     else:
+#         for i in guests:
+#             print(i)
+#         break
+# task 2
+# _str=input("enter something: ")
+# unique=set(_str)
+# print(f'length: {len(_str)}, unique symbols: {len(unique)}')
+
+# task 3
+# _list = ['a', 'b', 'a', 'c', 'g', 'd', 'g', 'e', 'f', 'g', 'i', 'v']
+# unique=set(_list)
+# print(unique)
+
+#task 4
+# guests=['Ратушный', 'Высоцкая', 'Иванова', 'Щепетков', 'Климчук']
+# while len(guests)>0:
+#     guest = input('enter guest: ')
+#     if guest in guests:
+#         guests.remove(guest)
+#         print('access permitted')
+#     else:
+#         print('access denied')
+# else:
+#     print('вход закрыт')
+
