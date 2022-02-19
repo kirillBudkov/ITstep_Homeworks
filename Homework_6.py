b5dcfd770d5d49aba9de8631bc84b38f09b240a3
kirill <kirill.s.b.88@gmail.com>
2022-02-19 21:49:36 +0200

    Homework 6 list and set task 2 upgrade

--- Homework_6.py
+++ Homework_6.py
@@ -21,18 +21,18 @@
 #         break
 
 #task 2_up
-# goods=['apple', "meet", 'butter', 'juice','milk']
-# backet=[]
-# while len(goods)>0:
-#         check=input("enter your product: ")
-#         if check in goods:
-#             print(f'Вы купили {check}')
-#             goods.remove(check)
-#             backet.append(check)
-#         elif check in backet:
-#             print(f'{check} уже купили')
-#         else:
-#             print(f'{check} в списке нет')
-# else:
-#     print('все продукты куплены')
+goods=['apple', "meet", 'butter', 'juice','milk']
+backet=[]
+while len(goods)>0:
+        check=input("enter your product: ")
+        if check in goods:
+            print(f'Вы купили {check}')
+            goods.remove(check)
+            backet.append(check)
+        elif check in backet:
+            print(f'{check} уже купили')
+        else:
+            print(f'{check} в списке нет')
+else:
+    print('все продукты куплены')
 
