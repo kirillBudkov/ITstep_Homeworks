e146e3eb97b7037c4885e407c1472e5d05289320
kirill <kirill.s.b.88@gmail.com>
2022-02-19 20:58:56 +0200

    Homework 6 Lists

---
+++ Homework_6.py
@@ -0,0 +1,23 @@
+# # task 1
+# _list=["a",1,4,6,7,'g','2']
+# count=0
+# for i in _list:
+#     count+=1
+# print(count)
+
+#
+# # #task 2
+# goods=['apple', "meet", 'butter', 'juice','milk']
+# while True:
+#     if len(goods)!=0:
+#         check=input("enter your product: ")
+#         if check in goods:
+#             print(f'Вы купили {check}')
+#             goods.remove(check)
+#         else:
+#             print(f'{check} в списке нет')
+#     else:
+#         print('все продукты куплены')
+#         break
+
+
